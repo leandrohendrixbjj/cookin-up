02ad6f4de0bb2c4503acc3da329e9eb337ea38db
hendrix <leandrotully@gmail.com>
2024-05-01 08:51:06 -0300

    3.1-)Ajuste simples

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-   import Banner from './Banner.vue';
+   import Banner from './components/Banner.vue';
    export default {
     components: {Banner}
    }
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="banner">
     <div class="apresentacao">
-      <img src="./assets/imagens/logo.svg" alt="Logo do Cookin' Up" class="logo">
+      <img src="../assets/imagens/logo.svg" alt="Logo do Cookin' Up" class="logo">
       
       <p class="cabecalho-lg frase-cabecalho">
        <span class="texto-verde">Um banquete de ideias para</span>
@@ -12,7 +12,7 @@
         Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
       </p>
     </div>
-    <img src="./assets/imagens/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="foto-banner">  
+    <img src="../assets/imagens/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="foto-banner">  
   </header>
 </template>
 
