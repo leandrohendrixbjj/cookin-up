c2855cd50263dd87cb6e1934d18b23dedaafa87c
hendrix <leandrotully@gmail.com>
2024-07-18 19:03:01 -0300

    8.3=>Ajustes

--- src/hook/updateHook.vue
+++ src/hook/updateHook.vue
@@ -3,6 +3,9 @@
     [beforeUpdate] => entra em execução imediatamente após uma propriedade reativa ser alterada, 
     mas imediatamente antes do componente ser re-renderizado em tela.
     Muito utilizado para operações de debugging para identificar em que momento a renderização do componente é ativada.
+
+    [updated] =>  é executado imediatamente após uma re-renderização do componente causado por uma alteração de 
+    dados reativos, sendo bastante útil para criação de efeitos colaterais (side effects) relacionados ao DOM.
     
     [mounted] => O mounted é executado imediatamente após o componente ser montado. Nesse estágio, o componente se 
     torna funcional: as propriedades de data() são injetadas no template, os elementos são renderizados e a 
@@ -16,9 +19,12 @@ export default {
       historico: []
     }
   },
-  beforeUpdate(){ 
-    alert("Welcome to BeforeUpdate")           
+  beforeUpdate(){     
+    alert("Welcome to BEFORE UPDATE")           
   },  
+  updated() {
+    alert("Welcome to UPDATE")    
+  },
   mounted() {       
     this.contador++      
   },
