038b73361e9dba64563faeb549f52b308d626dba
hendrix <leandrotully@gmail.com>
2024-08-26 19:30:30 -0300

    10-)Reutilizando CSS com componentes

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
   
   import SelecionarIngredientes from './SelecionarIngredientes.vue'
+  import Tag from './Tag.vue';
 
   export default {
     data(){
@@ -8,7 +9,7 @@
         ingredientes:['Alho', 'Manteiga', 'Orégano', 'Milho']
       }
     },
-    components: { SelecionarIngredientes }
+    components: { SelecionarIngredientes, Tag }
   }
 </script>
 
@@ -20,8 +21,8 @@
       </span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente" class="ingrediente">
-          {{ ingrediente }}
+        <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente">
+          <Tag :texto="ingrediente"/>
         </li>        
       </ul>
 
@@ -59,18 +60,6 @@
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-    transition: 0.2s;
-    color: var(--creme, #FFFAF3);
-  background: var(--coral, #F0633C);
-  font-weight: 700;
 }
 
 .lista-vazia {
